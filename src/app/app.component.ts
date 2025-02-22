514c87c2ad19310811f71e92401d042aceb7d19b
roddrigoqueiroz <rodrigo.queiroz.neves@gmail.com>
2025-02-22 13:57:58 -0300

    Setando projeto Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainPageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'RU_PDSI';
+}
---
+++ src/app/consulta-relatorios/consulta-relatorios.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-consulta-relatorios',
+  imports: [HeaderComponent, FooterComponent],
+  templateUrl: './consulta-relatorios.component.html',
+  styleUrl: './consulta-relatorios.component.css'
+})
+export class ConsultaRelatoriosComponent {
+
+}
