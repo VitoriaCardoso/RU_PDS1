044fcde752054fb47ce52de1f17734c023577d2f
Vitoria <mydream@MacBook-Pro-de-MyDream.local>
2025-03-13 22:14:14 -0300

    Criação da Pagina Grafico 2

--- src/app/grafico/grafico.component.ts
+++ src/app/grafico/grafico.component.ts
@@ -1,7 +1,6 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
-import Chart from 'chart.js/auto';
-
+import { Chart, ChartConfiguration, ChartData, ChartEvent, registerables } from 'chart.js/auto';
 
 @Component({
   selector: 'app-grafico',
@@ -10,29 +9,51 @@ import Chart from 'chart.js/auto';
   styleUrl: './grafico.css'
 })
 export class ConsultaGraficoComponent implements AfterViewInit {
-  @ViewChild('frequenciaChart') frequenciaChart!: ElementRef;
+  @ViewChild('chart1') chart1!: ElementRef;
+  @ViewChild('chart2') chart2!: ElementRef;
 
   ngAfterViewInit() {
-    const ctx = this.frequenciaChart.nativeElement.getContext('2d');
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['A', 'B', 'C', 'D', 'E'],
-        datasets: [{
-          label: 'Frequência',
-          data: [10, 25, 14, 30, 20],
-          backgroundColor: 'rgba(54, 162, 235, 0.6)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-        }],
-      },
-      options: {
-        responsive: true,
-        scales: {
-          y: { beginAtZero: true },
-        },
-      },
-    });
+    this.criarGrafico(this.chart1.nativeElement, 'Sétimo Ano');
+    this.criarGrafico(this.chart2.nativeElement, 'Nono Ano');
   }
-}
 
+  criarGrafico(canvas: HTMLCanvasElement, titulo: string) {
+    if (canvas) {
+      new Chart(canvas, {
+        type: 'line',
+        data: {
+          labels: ['2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018'],
+          datasets: [
+            {
+              label: 'Sétimo ano',
+              data: [10, 12, 15, 18, 22, 25, 24, 27, 30, 32],
+              borderColor: 'blue',
+              fill: false
+            },
+            {
+              label: 'Oitavo ano',
+              data: [12, 15, 18, 16, 20, 23, 25, 26, 30, 33, 35],
+              borderColor: 'red',
+              fill: false
+            },
+            {
+              label: 'Nono ano',
+              data: [20, 21, 19, 22, 25, 27, 28, 30, 33, 34, 35],
+              borderColor: 'yellow',
+              fill: false
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              display: true,
+              position: 'top'
+            }
+          }
+        }
+      });
+    }
+}}
