814f7811d1c8aea554b36fe87ad3310963af28a8
roddrigoqueiroz <rodrigo.queiroz.neves@gmail.com>
2025-03-25 19:49:31 -0300

    Refactor: Removendo import desnecessário

--- src/app/cardapio/cardapio.ts
+++ src/app/cardapio/cardapio.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-cardapio',
-  imports: [LogoComponent],
   templateUrl: './cardapio.html',
   styleUrl: './cardapio.css'
 })
