cfa357318fa1735f31a9ae1bc242b38e9236b282
roddrigoqueiroz <rodrigo.queiroz.neves@gmail.com>
2025-03-31 20:12:55 -0300

    Adicionando conexão com back-end

--- src/app/pagina-not-found/pagina-not-found.component.ts
+++ src/app/pagina-not-found/pagina-not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-pagina-not-found',
@@ -7,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-not-found.component.css'
 })
 export class PaginaNotFoundComponent {
+  data: any;
 
+  constructor(private http: HttpService) {}
+
+  ngOnInit() {
+    this.http.getData().subscribe({
+      next: (response) => {
+        console.log('API Response:', response.symbol + 'Valor: ' + response.price);
+        this.data = response;
+      },
+      error: (error) => {
+        console.error('API Error:', error);
+      }
+    });
+  }
 }
