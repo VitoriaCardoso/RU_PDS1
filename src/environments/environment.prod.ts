4a17a00df42bb02d6aa7fe434ff33321ddd5e494
roddrigoqueiroz <rodrigo.queiroz.neves@gmail.com>
2025-04-27 16:57:51 -0300

    fix: Adicionando ip do back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.yourproductiondomain.com' // Production URL
+    apiUrl: 'http://44.211.255.182:8080' // Production URL
 };
